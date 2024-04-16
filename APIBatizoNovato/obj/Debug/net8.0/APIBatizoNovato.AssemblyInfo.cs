80338081e4312a30a9e9a9bb4baafdb9bb1334de
EduRRamoss <eduardorodriguesr@outlook.com>
2024-04-16 10:32:44 -0300

    Branch setup inicial

--- APIBatizoNovato/obj/Debug/net8.0/APIBatizoNovato.AssemblyInfo.cs
+++ APIBatizoNovato/obj/Debug/net8.0/APIBatizoNovato.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIBatizoNovato")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00fe7f59a7724435027f7badb31c461a94845f32")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIBatizoNovato")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIBatizoNovato")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
